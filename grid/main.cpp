8abf8b0b9710486da8a6ff09a494246b4b01ad08
BaoBao Nguyen <bbao2019@chickenfeet.local>
2021-03-24 14:19:58 -0400

    Monkey Code

--- grid/main.cpp
+++ grid/main.cpp
@@ -11,35 +11,78 @@
 using namespace std;
 using namespace mssm;
 
+Vec2d rowColtoXY(Graphics& g, int boxWidth, int boxHeight, int row, int column){
+
+    int columnWidth = boxWidth + 0;
+    int rowHeight = boxHeight+10;
+    int topMargin = 10;
+    int leftMargin = 10;
+
+    int x = column*columnWidth + leftMargin;
+    int y = g.height() - (boxHeight+topMargin) - rowHeight*row;
+
+    return {x,y};
+}
+
+Vec2d xyToRowCol(Graphics& g, int boxWidth, Vec2d pos, int boxHeight){
+
+    int topMargin = 10;
+    int leftMargin = 10;
+    int columnWidth = boxWidth + 0;
+    int rowHeight = boxHeight+10;
+
+    int row = (pos.y + topMargin - g.height())/(-rowHeight);
+    int col = (pos.x - leftMargin)/columnWidth;
+
+    return {col, row};
+}
+
+
+
 void graphicsMain(Graphics& g)
 {
+
+    int numColumns =  10;
+    int numRows = 10;
+    int boxWidth = 30;
+    int boxHeight = 30;
+
+    vector<vector<bool>> grid(numRows, vector<bool>(numColumns, false));
+
     while (g.draw()) {
         g.clear();
 
-        int boxWidth = 30;
-        int boxHeight = 30;
-        int numRows = 10;
-        int columnWidth = 30;
-        int rowHeight = boxHeight+10;
-        int topMargin = 10;
-        int leftMargin = 10;
 
-        int numColumns = (g.width()-2*leftMargin)/columnWidth;
+
+        //        (g.width()-2*leftMargin)/columnWidth;
 
 
         for (int column = 0; column < numColumns; column++){
             for (int row = 0; row <numRows; row++){
-                int x = column*columnWidth + leftMargin;
-                int y = g.height() - (boxHeight+topMargin) - rowHeight*row;
-                g.rect ({x, y}, boxWidth, boxHeight, GREEN);
+                Color fillColor = BLACK;
+                if(grid[row][column] == true){
+                    fillColor = BLUE;
+                }
+                g.rect (rowColtoXY(g, boxWidth, boxHeight, row, column), boxWidth, boxHeight, GREEN, fillColor);
             }
         }
+        //        Vec2d rc = xyToRowCol(g, boxWidth, boxHeight, g.mousePos());
+        //        int currentRow = rc.y;
+        //        int currentCol = rc.x;
 
+
+        //        g.text({10,10}, 20, to_string(currentRow) + " " + to_string(currentCol));
         for (const Event& e : g.events())
         {
             switch (e.evtType)
             {
-            case EvtType::MousePress:
+            case EvtType::MousePress:{
+
+                Vec2d rc = xyToRowCol(g, boxWidth, g.mousePos(), boxHeight);
+                if (rc.y >= 0 && rc.y < numRows && rc.x >= 0 && rc.x < numColumns){
+                    grid[rc.y][rc.x] = !grid[rc.y][rc.x];
+                }
+            }
                 break;
             case EvtType::MouseRelease:
                 break;
